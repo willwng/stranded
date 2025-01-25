Fix gradient calculation in bend-twist

energies/bend_twist.py
@@ -23,16 +23,8 @@ class BendTwist(Energy):
     @staticmethod
     def d_energy_d_pos(pos: np.ndarray, theta: np.ndarray, solver_params: SolverParams):
         n = solver_params.n
-        # First we compute partial E / partial theta_n
-        l_bar_n = solver_params.l_bar[n]
-        omega_nn = Bend.compute_omega(pos, theta, n, n, solver_params)
-        omega_bar_nn = solver_params.omega_bar[n, 1]
-        B_n = solver_params.B[n]
-        m_n = theta[n] - theta[n - 1]
-        d_energy_d_theta_n = (1 / l_bar_n) * (
-                np.dot(omega_nn, Vector.J @ (B_n @ (omega_nn - omega_bar_nn))) + 2 * solver_params.beta * m_n)
 
-        # Compute partial E / partial x_n
+        # Compute partial E / partial x_i, for all nodes
         d_energy_d_pos = np.zeros_like(pos)
         for i in range(n + 2):
             # k from 1 to n
@@ -45,33 +37,55 @@ class BendTwist(Energy):
                     omega_bar_kj = solver_params.omega_bar[k, j_idx]
                     d_omega_kj = omega_kj - omega_bar_kj
 
-                    # Compute m_1j and m_2j
+                    # Compute nabla_i omega_kj. Requires m_1j, m_2j
                     theta_j = theta[j]
                     u, v = solver_params.bishop_frame[j]
                     m_1j = np.cos(theta_j) * u + np.sin(theta_j) * v
                     m_2j = -np.sin(theta_j) * u + np.cos(theta_j) * v
-                    nabla_i_kb_i = BendTwist.nabla_i_kb_i(pos, k)
                     m_T = np.array([m_2j, -m_1j])
+                    # Now compute nabla_i (kb)_k
+                    nabla_i_kb_k = BendTwist.nabla_kb(pos=pos, i=k, d=i - k, solver_params=solver_params)
+                    # nabla_i psi_j
                     nabla_i_psi_j = BendTwist.nabla_i_psi_j(pos, i, j, solver_params)
-                    nabla_i_omega_kj = m_T @ nabla_i_kb_i - Vector.J @ np.outer(omega_kj, nabla_i_psi_j)
+                    nabla_i_omega_kj = m_T @ nabla_i_kb_k - Vector.J @ np.outer(omega_kj, nabla_i_psi_j)
                     grad = den * nabla_i_omega_kj.T @ (B_j @ d_omega_kj)
                     d_energy_d_pos[i] += grad
 
-        # Second term of d E / d x_n
+        # Second term of d E / d x_i = -partial E / partial theta_n * partial theta_n / partial x_i
+        l_bar_n = solver_params.l_bar[n]
+        omega_nn = Bend.compute_omega(pos, theta, n, n, solver_params)
+        omega_bar_nn = solver_params.omega_bar[n, 1]
+        B_n = solver_params.B[n]
+        m_n = theta[n] - theta[n - 1]
+        d_energy_d_theta_n = (1 / l_bar_n) * (
+                np.dot(omega_nn, Vector.J @ (B_n @ (omega_nn - omega_bar_nn))) + 2 * solver_params.beta * m_n)
         for i in range(n + 2):
             d_energy_d_pos[i] -= d_energy_d_theta_n * BendTwist.nabla_i_psi_j(pos, i, n, solver_params)
         return d_energy_d_pos
 
     @staticmethod
-    def nabla_i_kb_i(pos: np.ndarray, i: int):
-        """ Computes the gradient of the curvature binormal """
+    def nabla_kb(pos: np.ndarray, i: int, d: int, solver_params: SolverParams):
+        """
+        Compute nabla_{i-1}(kb)_i, nabla_i(kb)_i, nabla_{i+1}(kb)_i
+            represented by d [-1, 0, 1], respectively
+        """
+        # Only nabla_{i-1}, nabla_i, nabla_{i+1} are non-zero
+        if d not in [-1, 0, 1]:
+            return np.zeros((3, 3))
+        # First compute (kb)_i, and denominator of the gradients
         e_i, e_im1 = (pos[i + 1] - pos[i]), (pos[i] - pos[i - 1])
-        kb_i = np.cross(2 * e_im1, e_i)
-        kb_i /= (np.linalg.norm(e_im1) * np.linalg.norm(e_i) + np.dot(e_im1, e_i))
-        den = np.linalg.norm(e_im1) * np.linalg.norm(e_i) + np.dot(e_im1, e_i)
+        l_bar_i, l_bar_im1 = solver_params.l_bar_edge[i], solver_params.l_bar_edge[i - 1]
+        kb_i = Bend.compute_curvature_binormal(pos, i, solver_params)
+        den = l_bar_i * l_bar_im1 + np.dot(e_im1, e_i)
+
+        # Gradients of curvature binormal
         nabla_im1 = (2 * Vector.skew_sym(e_i) + np.outer(kb_i, e_i)) / den
         nabla_ip1 = (2 * Vector.skew_sym(e_im1) - np.outer(kb_i, e_im1)) / den
         nabla_i = -(nabla_im1 + nabla_ip1)
+        if d == -1:
+            return nabla_im1
+        elif d == 1:
+            return nabla_ip1
         return nabla_i
 
     @staticmethod
@@ -82,9 +96,7 @@ class BendTwist(Energy):
             return 0.0
 
         # First compute the curvature binormal
-        e_i, e_im1 = (pos[i + 1] - pos[i]), (pos[i] - pos[i - 1])
-        kb_i = np.cross(2 * e_im1, e_i)
-        kb_i /= (np.linalg.norm(e_im1) * np.linalg.norm(e_i) + np.dot(e_im1, e_i))
+        kb_i = Bend.compute_curvature_binormal(pos, i, solver_params)
 
         # Gradients given by Eq. 9
         if d == 0:
@@ -103,7 +115,7 @@ class BendTwist(Energy):
     @staticmethod
     def nabla_i_psi_j(pos: np.ndarray, i: int, j: int, solver_params: SolverParams):
         """ Computes the gradient of Psi_j wrst i """
-        grad = 0.0
+        grad = np.zeros(3)
         # From k = 1 to j
         for k in range(1, j + 1):
             grad += BendTwist.nabla_di_psi_i(pos, k, i - k, solver_params)

energies/bending.py
@@ -42,17 +42,18 @@ class Bend(Energy):
         return d_energy_d_theta
 
     @staticmethod
-    def compute_curvature_binormal(pos: np.ndarray, i: int):
+    def compute_curvature_binormal(pos: np.ndarray, i: int, solver_params: SolverParams):
         """ Computes the curvature binormal vector at vertex i """
         e_i, e_im1 = (pos[i + 1] - pos[i]), (pos[i] - pos[i - 1])
         kb_i = np.cross(2 * e_im1, e_i)
-        kb_i /= (np.linalg.norm(e_im1) * np.linalg.norm(e_i) + np.dot(e_im1, e_i))
+        l_bar_i, l_bar_im1 = solver_params.l_bar_edge[i], solver_params.l_bar_edge[i - 1]
+        kb_i /= (l_bar_i * l_bar_im1 + np.dot(e_im1, e_i))
         return kb_i
 
     @staticmethod
     def compute_omega(pos: np.ndarray, theta: np.ndarray, i: int, j: int, solver_params: SolverParams):
         """ Computes the material curvature """
-        kb_i = Bend.compute_curvature_binormal(pos, i)
+        kb_i = Bend.compute_curvature_binormal(pos, i, solver_params)
         # Compute the material frame
         u, v = solver_params.bishop_frame[j]
         theta_j = theta[j]

energies/twisting.py
@@ -7,13 +7,12 @@ import numpy as np
 class Twist(Energy):
     @staticmethod
     def compute_energy(pos: np.ndarray, theta: np.ndarray, solver_params: SolverParams):
-        energy = 0.0
         # From edge 1 to edge n
-        for i in range(1, solver_params.n + 1):
-            theta_i, theta_im1 = theta[i], theta[i - 1]
-            l_i = solver_params.l_bar[i]
-            m_i = theta_i - theta_im1
-            energy += solver_params.beta * m_i ** 2 / l_i
+        ind = np.arange(1, solver_params.n + 1)
+        theta_i, theta_im1 = theta[ind], theta[ind - 1]
+        l_i = solver_params.l_bar[ind]
+        m_i = theta_i - theta_im1
+        energy = np.sum(solver_params.beta * m_i ** 2 / l_i)
         return energy
 
     @staticmethod

main.py
@@ -40,18 +40,18 @@ def main():
     mass = np.ones(n_vertices) / n_vertices * 0.1
     beta = 0.1
     k = 25.0
-    dt = 0.05
+    dt = 0.01
     g = 9.81
 
     # rod.theta *= 0.1
     # rod.pos *= 1.2
     create_frame(rod, 0)
 
-    energies = [Gravity()]
+    energies = [Bend(), Twist(), BendTwist(), Gravity()]
     p_top = rod.pos[-1]
     Sim.init(rod, B, beta, k, g, p_top, mass, energies, dt)
     for i in range(500):
-        if i % 10 == 0:
+        if i % 50 == 0:
             print(f"iteration {i}")
             create_frame(rod, i)
         Sim.step(rod, B, beta, k, g, p_top, mass, energies, dt)

solver/centerline_integrator.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from constraints.clamp import Clamp
-from constraints.inextensibility import Inextensibility
 from energies.energy import Energy
 from solver.solver_params import SolverParams
 
@@ -14,6 +12,35 @@ class CenterlineIntegrator:
         forces = np.zeros_like(pos)
         for energy in energies:
             forces -= energy.d_energy_d_pos(pos, theta, solver_params)
+
+        def total_energy(p):
+            p = p.reshape(-1, 3)
+            energy = 0
+            for e in energies:
+                energy += e.compute_energy(p, theta, solver_params)
+            return energy
+
+        def total_grad_energy(p):
+            p = p.reshape(-1, 3)
+            grad_energy = np.zeros_like(p)
+            for e in energies:
+                grad_energy += e.d_energy_d_pos(p, theta, solver_params)
+            return grad_energy.flatten()
+        #
+        # print("analytical gradient: ", total_grad_energy(pos.flatten()))
+        # # Compute finite diff
+        # h = 1e-6
+        # grad_fd = np.zeros_like(pos)
+        # for i in range(pos.shape[0]):
+        #     for j in range(pos.shape[1]):
+        #         pos[i, j] += h
+        #         energy_plus = total_energy(pos)
+        #         pos[i, j] -= 2 * h
+        #         energy_minus = total_energy(pos)
+        #         pos[i, j] += h
+        #         grad_fd[i, j] = (energy_plus - energy_minus) / (2 * h)
+        # print("finite difference gradient: ", grad_fd.ravel())
+        # quit()
 
         # Predicted position, with no constraints
         M_inv = np.diag(1 / solver_params.mass)

solver/sim.py
@@ -36,7 +36,7 @@ class Sim:
 
     @staticmethod
     def init(rod: Rod, B: np.ndarray, beta: float, k: float, g: float, p_top, mass: np.ndarray, energies: list[Energy], dt):
-        # Set up the solver
+        """ Initialization steps of simulation """
         solver_params = SolverParams(
             B=B,
             beta=beta,