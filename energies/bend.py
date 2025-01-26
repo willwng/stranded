ba8bd6c7a4bf63bc435a3b8fdef7d2e8aa88ee97
William Wang <wyw1@stanford.edu>
2025-01-25 23:05:37 -0800

    Vectorize bend gradient

--- energies/bend.py
+++ energies/bend.py
@@ -26,29 +26,28 @@ class Bend(Energy):
     @staticmethod
     def d_energy_d_theta(grad: np.ndarray, pos: np.ndarray, theta: np.ndarray, rod_state: RodState,
                          init_rod_state: InitialRodState, rod_params: RodParams):
-        # TODO: vectorize
-        # All edges
-        n = theta.shape[0] - 1
+        l_bar = init_rod_state.l_bar
+        omega_bar = init_rod_state.omega_bar
+
         omega = RodUtil.compute_omega(theta=theta, curvature_binormal=rod_state.kb, bishop_frame=rod_state.bishop_frame)
-        for j in range(0, n + 1):
-            l_j = init_rod_state.l_bar[j]
-            B_j = rod_params.B[j]
+        d_omega = omega - omega_bar
 
-            # First term
-            if j != 0:
-                omega_jj = omega[j, 1]
-                omega_bar_jj = init_rod_state.omega_bar[j, 1]
-                grad[j] += (1 / l_j) * np.dot(omega_jj, Vector.J @ (B_j @ (omega_jj - omega_bar_jj)))
+        # All edges except the first
+        omega1 = omega[:, 1, :]
+        d_omega1 = d_omega[:, 1, :]
+        B_d_omega1 = np.einsum('ijk,ik->ij', rod_params.B, d_omega1)
+        J_B_d_omega1 = np.dot(B_d_omega1, Vector.J.T)
+        grad[1:] += (1 / l_bar[1:]) * np.einsum('ij,ij->i', J_B_d_omega1, omega1)[1:]
 
-            # Second term, only if not last edge
-            if j != n:
-                l_jp1 = init_rod_state.l_bar[j + 1]
-                omega_jp1j = omega[j + 1, 0]
-                omega_bar_jp1j = init_rod_state.omega_bar[j + 1, 0]
-                grad[j] += (1 / l_jp1) * np.dot(omega_jp1j, Vector.J @ (B_j @ (omega_jp1j - omega_bar_jp1j)))
+        # All edges except the last
+        omega0 = omega[:, 0, :]
+        d_omega0 = d_omega[:, 0, :]
+        B_d_omega0 = np.einsum('ijk,ik->ij', rod_params.B, d_omega0)
+        J_B_d_omega0 = np.dot(B_d_omega0, Vector.J.T)
+        grad[:-1] += (1 / l_bar[1:]) * np.einsum('ij,ij->i', J_B_d_omega0, omega0)[1:]
         return grad
 
     @staticmethod
     def d_energy_d_pos(grad: np.ndarray, pos: np.ndarray, theta: np.ndarray, rod_state: RodState,
-                         init_rod_state: InitialRodState, rod_params: RodParams):
+                       init_rod_state: InitialRodState, rod_params: RodParams):
         return grad  # Handled in BendTwist
--- energies/twist.py
+++ energies/twist.py
@@ -15,13 +15,11 @@ class Twist(Energy):
     @staticmethod
     def d_energy_d_theta(grad: np.ndarray, pos: np.ndarray, theta: np.ndarray, rod_state: RodState,
                          init_rod_state: InitialRodState, rod_params: RodParams):
-        l = init_rod_state.l_bar
+        l_bar = init_rod_state.l_bar
         beta = rod_params.beta
-        grad[1:-1] += 2 * beta * (theta[1:-1] - theta[:-2]) / l[1:-1]
-        grad[1:-1] -= 2 * beta * (theta[2:] - theta[1:-1]) / l[2:]
-        # First and last edge
-        grad[0] -= 2 * beta * (theta[1] - theta[0]) / l[1]
-        grad[-1] += 2 * beta * (theta[-1] - theta[-2]) / l[-1]
+        # All but first edge, all but last theta
+        grad[1:] += 2 * beta * (theta[1:] - theta[:-1]) / l_bar[1:]
+        grad[:-1] -= 2 * beta * (theta[1:] - theta[:-1]) / l_bar[1:]
         return grad
 
     @staticmethod
