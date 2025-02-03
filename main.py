8c8f24e7223c5108b0cd82a3722ec67165cc1315
William Wang <wyw1@stanford.edu>
2025-02-03 12:19:45 -0800

    Make visualization depend on rod parameters

--- main.py
+++ main.py
@@ -13,21 +13,14 @@ from solver.solver_params import update_csv_analytics
 from visualization.visualizer import Visualizer
 
 
-def create_frame(pos: np.ndarray, theta: np.ndarray, material_frame: np.ndarray, i: int):
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # Visualizer.draw_nodes(pos=pos, ax=ax)
-    # Visualizer.draw_edges(pos=pos, ax=ax)
-    # # if bishop_frame is not None:
-    # #     Visualizer.draw_material_frame(pos=pos, theta=theta, bishop_frame=bishop_frame, ax=ax)
-    # Visualizer.set_lims(pos=pos, ax=ax)
-    # plt.title(f"t={i * 0.04:.2f}")
-    # plt.savefig(f"output/frames/frame_{i}.png")
-    # plt.close()
-
-    # Visualizer.to_obj(pos=pos, output_file=f"output/obj/obj_{i}.obj")
-    Visualizer.strand_to_obj(pos=pos, material_frame=material_frame, output_file=f"output/obj/obj_{i}.obj",
-                             point_radius=0.1, axis_1_radius=0.2, axis_2_radius=0.1)
+def create_frame(pos: np.ndarray,
+                 material_frame: np.ndarray,
+                 point_radii: np.ndarray,
+                 ax1_radii: np.ndarray,
+                 ax2_radii: np.ndarray,
+                 i: int):
+    Visualizer.strand_to_obj(pos=pos, material_frame=material_frame, point_radii=point_radii, ax1_radii=ax1_radii,
+                             ax2_radii=ax2_radii, output_file=f"output/obj/obj_{i}.obj")
     return
 
 
@@ -36,19 +29,26 @@ def main():
     n_points = 30
     pos, theta = RodGenerator.example_rod(n_points)
 
-    # Elastic properties
+    # Define properties of the rod
     n_vertices, n_edges = pos.shape[0], theta.shape[0]
+
+    # Just set the mass to 1 for now
+    mass = np.ones(n_vertices) * 1
+
     # For each edge, the bending stiffness matrix
     B = np.zeros((n_edges, 2, 2))
     for i in range(n_edges):
-        B[i, 0, 0] = 4.0
+        B[i, 0, 0] = 8.0
         B[i, 1, 1] = 1.0
+
+    # Visualization parameters
+    obj_scale = 0.1
+    point_radii = np.cbrt(mass) * obj_scale
+    ax1_radii = np.sqrt(B[:, 0, 0]) * point_radii[1:]
+    ax2_radii = np.sqrt(B[:, 1, 1]) * point_radii[1:]
 
     # Twisting stiffness
     beta = 0.1
-
-    # Just set the mass to 1 for now
-    mass = np.ones(n_vertices)
 
     # Stretching stiffness (unused)
     k = 0.0
@@ -65,25 +65,29 @@ def main():
     sim = Sim(pos=pos, theta=theta, B=B, beta=beta, k=k, g=g, mass=mass, energies=energies,
               damping=damping, dt=dt, xpbd_steps=xpbd_steps)
 
-    create_frame(pos, theta, sim.state.bishop_frame, 0)
+    material_frame = sim.state.material_frame
+    create_frame(pos=pos, material_frame=material_frame, point_radii=point_radii, ax1_radii=ax1_radii,
+                 ax2_radii=ax2_radii, i=0)
     sim.update_analytics(pos, theta)
     update_csv_analytics(sim.analytics, f"output/analytics.csv")
 
     start = time.time()
 
     # Straighten the rod (keeping edge lengths constant)
-    edge_lengths = np.linalg.norm(pos[1:] - pos[:-1], axis=1)
-    for i in range(1, n_points + 2):
-        pos[i] = np.array([0, 0, pos[0, 2] - np.sum(edge_lengths[:i])], dtype=np.float64)
+    # edge_lengths = np.linalg.norm(pos[1:] - pos[:-1], axis=1)
+    # for i in range(1, n_points + 2):
+    #     pos[i] = np.array([0, 0, pos[0, 2] - np.sum(edge_lengths[:i])], dtype=np.float64)
 
-    create_frame(pos, theta, sim.state.material_frame, 1)
+    create_frame(pos=pos, material_frame=material_frame, point_radii=point_radii, ax1_radii=ax1_radii,
+                 ax2_radii=ax2_radii, i=1)
 
     # Run the simulation
     save_freq = 10
     progress = tqdm(range(2 * save_freq, 10000))
     for i in progress:
         if i % save_freq == 0:
-            create_frame(pos, theta, sim.state.material_frame, i // save_freq)
+            create_frame(pos=pos, material_frame=sim.state.material_frame, point_radii=point_radii, ax1_radii=ax1_radii,
+                         ax2_radii=ax2_radii, i=i // save_freq)
             progress.set_description(f"Frame {i // save_freq}")
             update_csv_analytics(sim.analytics, f"output/analytics.csv")
         pos, theta = sim.step(pos=pos, theta=theta)
