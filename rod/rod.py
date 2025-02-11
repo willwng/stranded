58703fb21a854ba4f0fdaa7366d082a0b922c1db
William Wang <wyw1@stanford.edu>
2025-02-10 16:06:51 -0800

    Update sim features

--- rod/rod.py
+++ rod/rod.py
@@ -33,6 +33,35 @@ class RodState:
     frozen_theta_indices: np.ndarray
 
 
+def empty_rod_state(n_vertices: int, frozen_pos_indices: np.ndarray, frozen_theta_indices: np.ndarray) -> RodState:
+    n_edges = n_vertices - 1
+    return RodState(
+        vel=np.zeros((n_vertices, 3)),
+        bishop_frame=np.zeros((n_edges, 2, 3)),
+        kb=np.zeros((n_edges, 3)),
+        kb_den=np.zeros(n_edges),
+        nabla_kb=np.zeros((n_edges, 3, 3, 3)),
+        nabla_psi=np.zeros((n_edges, 3, 3)),
+        material_frame=np.zeros((n_edges, 2, 3)),
+        frozen_pos_indices=frozen_pos_indices,
+        frozen_theta_indices=frozen_theta_indices,
+    )
+
+
+def copy_rod_state(state: RodState) -> RodState:
+    return RodState(
+        vel=state.vel.copy(),
+        bishop_frame=state.bishop_frame.copy(),
+        kb=state.kb.copy(),
+        kb_den=state.kb_den.copy(),
+        nabla_kb=state.nabla_kb.copy(),
+        nabla_psi=state.nabla_psi.copy(),
+        material_frame=state.material_frame.copy(),
+        frozen_pos_indices=state.frozen_pos_indices.copy(),
+        frozen_theta_indices=state.frozen_theta_indices.copy(),
+    )
+
+
 @dataclass
 class InitialRodState:
     # Computed only once
