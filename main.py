37a26e662efff8074e8d975c5e5ba0537869f46f
William Wang <wyw1@stanford.edu>
2025-02-18 15:55:39 -0800

    push all for now

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from energies.bend_twist import BendTwist
 from energies.gravity import Gravity
 from energies.random import RandomForce
 from energies.twist import Twist
-from math_util.rotation import RotationUtil
+from math_util.rotation import RotationUtil, Quaternion
 from rod.RodHelixConverter import RodHelixConverter
 from rod.helix import Helix
 from rod.helix_util import HelixUtil
@@ -57,6 +57,8 @@ def create_frame_helix(helix: Helix, point_radii: np.ndarray, ax1_radii: np.ndar
 
 
 def plot_generalized_coords(helix: Helix):
+    import scienceplots
+    plt.style.use(["science", "nature"])
     plt.figure()
     twist = helix.q[::3]
     bend1 = helix.q[1::3]
@@ -65,6 +67,11 @@ def plot_generalized_coords(helix: Helix):
     plt.plot(i, twist, label="Twist")
     plt.plot(i, bend1, label="Bend 1")
     plt.plot(i, bend2, label="Bend 2")
+    plt.xlabel("Index")
+    plt.xlim([0, helix.n_sites])
+    plt.xticks([0, 35, 70])
+    plt.yticks([-1, 0, 1])
+
     plt.legend()
     plt.show()
 
@@ -199,5 +206,101 @@ def main():
         f.write(f"distance: {dis}")
 
 
+def expt():
+    n_pts = 51  # Including index 0
+    L = 30
+    s = np.linspace(0, L, n_pts)
+    # Generalized coordinates
+    curl_radius_mean, curl_radius_std = 0.4, 0.1  # 4mm +/- 1mm
+    curl_radius = np.random.normal(curl_radius_mean, curl_radius_std, n_pts)
+    delta_h = 1.0
+    k_1 = 1 / curl_radius
+    k_2 = np.random.normal(0, 0.1, n_pts)
+    tau = delta_h / (2 * np.pi * curl_radius_mean ** 2) * np.ones(n_pts)
+
+    # Randomize twist
+    # avg_num_cm_random = 3 * L / n_pts  # Try to randomize every [avg_num_cm_random] cm
+    # num_cm_random = int(L / avg_num_cm_random)
+    # random_idx = np.random.choice(n_pts, num_cm_random, replace=True)
+    # delta_tau = np.random.normal(0, 2.5, num_cm_random)
+    # tau[random_idx] += delta_tau
+
+    q = np.stack([tau, k_1, k_2], axis=1).ravel()
+    # Boundary/initial conditions
+    r0 = np.array([0, 0, 0])
+    n0 = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
+    # Stiffness, mass constants. Revisit this
+    EI = np.ones(3 * n_pts) * 1
+    rhoS = 0.05
+    g = 9.81 * 1e-3
+    helix = Helix(q=q, q0=q.copy(), n_sites=n_pts, s=s, L=L, r0=r0, n0=n0, EI=EI)
+
+    plot_generalized_coords(helix)
+
+    # Drawing parameters
+    point_radii = 0.05 * np.ones(n_pts)
+    ax1_radii = 0.05 * np.ones(n_pts)
+    ax2_radii = 0.05 * np.ones(n_pts)
+    point_style = ["sphere"] * n_pts
+    # create_frame_helix(helix, point_radii, ax1_radii, ax2_radii, point_style, frame_idx=0)
+
+    pos, theta = RodHelixConverter.helix_to_rod(helix)
+
+    centerline = pos[-1] - pos[0]
+    centerline = centerline / np.linalg.norm(centerline)
+    z_axis = np.array([0, 0, 1])
+    rot_axis = np.cross(centerline, z_axis)
+    rot_axis = rot_axis / np.linalg.norm(rot_axis)
+    rot_angle = np.arccos(np.dot(centerline, z_axis))
+    P_i = Quaternion.from_angle_axis(rot_angle, rot_axis)
+    P_i.normalize()
+    for i in range(pos.shape[0]):
+        pos[i] = P_i.rotate_vec(pos[i])
+
+    bishop_frame = RodUtil.compute_bishop_frames(pos=pos)
+    material_frame = RodUtil.compute_material_frames(theta=theta, bishop_frame=bishop_frame)
+    create_frame(pos=pos, material_frame=material_frame, point_radii=point_radii, ax1_radii=ax1_radii,
+                 ax2_radii=ax2_radii, point_style=point_style, frame_idx=0, draw_arrows=True)
+
+    # Reverse pos and theta
+    pos = pos[::-1]
+    theta = theta[::-1]
+
+    # -- simulate
+    mass = np.ones(n_pts) * 1.0
+    n_edges = theta.shape[0]
+    B = np.zeros((n_edges, 2, 2))
+    for i in range(n_edges):
+        B[i, 0, 0] = 1.0
+        B[i, 1, 1] = 1.0
+
+    # Twisting stiffness
+    beta = 1.0
+    k = 0.0
+
+    # Simulation parameters (damping for integration, time step, and number of XPBD steps)
+    damping = 0.1
+    dt = 0.04
+    xpbd_steps = 10
+    frozen_pos_indices = np.array([-1, -2, -3], dtype=int)
+    frozen_theta_indices = np.array([], dtype=int)
+
+    energies = [Twist(), Bend(), BendTwist(), Gravity()]
+    sim = Sim(pos=pos, theta=theta, B=B, beta=beta, k=k, g=g, mass=mass, energies=energies, damping=damping,
+              dt=dt, xpbd_steps=xpbd_steps, frozen_pos_indices=frozen_pos_indices,
+              frozen_theta_indices=frozen_theta_indices)
+    sim.define_rest_state(pos, theta)
+    save_freq = 10
+    progress = tqdm(range(1 * save_freq, 10000))
+    for i in progress:
+        if i % save_freq == 0:
+            create_frame(pos=pos, material_frame=sim.state.material_frame, point_radii=point_radii, ax1_radii=ax1_radii,
+                         ax2_radii=ax2_radii, point_style=point_style, frame_idx=i // save_freq)
+            progress.set_description(f"Frame {i // save_freq}")
+        pos, theta = sim.step(pos=pos, theta=theta)
+    return
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    expt()
